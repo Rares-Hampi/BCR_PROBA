68ddeac9cf91f69e85f09fb27e8714c62e329cb7
Hampi Rares <hampimarian1323@gmail.com>
2022-12-11 00:29:05 +0200

    merge api-ul

--- backend/api_test.js
+++ backend/api_test.js
@@ -1,34 +1,14 @@
-/*
- * get2
- *
- * get
- */
-const axios = require('axios');
-// const e = require('express');
+const axios = require("axios");
 
-// const get = async () => {
-//   const { data } = await axios.get("http://localhost:3000", {
-//     headers: {
-//       accept: "aplication/json",
-//     }js
-//   })
-//   return data;
-
-// }
-// console.log(get())
-const func = ()=>{
-axios.get("http://localhost:3000")
-  .then(resp => {
+axios
+  .get("http://localhost:3000/serviceResponse")
+  .then((resp) => {
     data = resp.data;
-    data.forEach(e => {
-        console.log(`${e.brn}`)
-
+    data.forEach((elem) => {
+      console.log(`${elem.br_street}`);
     });
-
   })
 
-  .catch(error => {
-      console.log(error);
-
-  });}
-  func()
+  .catch((error) => {
+    console.log(error);
+  });
